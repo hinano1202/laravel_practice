a9815e921570e5d83c10dc923d9fa67495ec8c60
hinano <ec2-user@ip-172-31-37-81.ec2.internal>
2020-04-07 06:48:23 +0000

    php14

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\News;
 
 class NewsController extends Controller
 
@@ -15,7 +16,34 @@ class NewsController extends Controller
 
 
 public function create(Request $request){
-    //news/createへリダイレクト
+    
+    //valitation
+    $this->validate($request, News::$rules);
+    $news = new News;
+    $form = $request->all();
+    
+    //画像があったら保存、なかったらnullを返す
+    if(isset($form['image'])){
+        //$formの中にimageはある？
+        $path = $request -> file('image') ->store('public/image');
+        //ある場合、imageをアップロードして/public/image/'ファイル名'というパスで保存
+        $news -> image_path = basename($path);
+        //Newsテーブルのimage_pathに、imageを保存したパスの最後、'ファイル名'の部分だけを書き込む
+        
+    } else {
+        $news -> image_path = null;
+        //imageがない場合、image_pathにはnullを返す
+    }
+    
+    unset($form['_token']);
+    unset($form['image']);
+    //一緒にformにくっついてた2項目を削除
+    
+    $news->full('form');
+    //Newsテーブルの中にformの内容を代入
+    $news->save();
+    //保存
+    
     return redirect('admin/news/create');
 }
 
